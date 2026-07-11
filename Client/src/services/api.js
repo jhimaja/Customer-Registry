ac12c92994cf734a6706020483589d72125b7526
jhimaja <jhimaja7286@gmail.com>
2026-07-11 18:00:37 +0530

    Updated API URL for production

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "baseURL: https://customer-care-backend-77zv.onrender.com/api",
 });
 
 // Automatically attach JWT token
