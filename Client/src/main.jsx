84a92a6f47354db72e6862fe7107128084984f7a
jhimaja <jhimaja7286@gmail.com>
2026-07-11 22:13:16 +0530

    Fix Profile component filename casing

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,5 @@
 import { ToastContainer } from "react-toastify";
-import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
